dde455ebb9a9401ebece449538d5ddac39e16b91
xdecock <xdecock@gmail.com>
2009-02-02 20:46:25 +0100

    So, the DKIM Infrastructure should be alright...
    (Or it's just a joke and it will wipe your hd)

--- lib/classes/Swift/Mime/SimpleMimeEntity.php
+++ lib/classes/Swift/Mime/SimpleMimeEntity.php
@@ -603,7 +603,7 @@ class Swift_Mime_SimpleMimeEntity implements Swift_Mime_MimeEntity
   /**
    * Get the KeyCache used in this entity.
    */
-  protected function _getCache()
+  protected function c()
   {
     return $this->_cache;
   }
